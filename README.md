7dc5608ca0b2d45be56ac1b9d4c7e4eb9030977b
Capt-Quail <146376347+Capt-Quail@users.noreply.github.com>
2024-03-24 19:37:42 -0400

    Source code generated
    Source code for the game has been generated. Classes ExtraterrestrialIncursion and Settings have been generated. Assets folder has been generated.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # extraterrestrial-incursion
-The process and execution of utilizing Pygame to focus on higher-level logic and create a basic game. 
+
+The process and execution of utilizing Pygame to focus on higher-level logic 
+and create a basic game. 
+
+In Extraterrestrial Incursion, the player controls a rocket ship that appears
+at the bottom of the screen. The player can move the ship right and left using
+arrow keys and shoot bulletts using the spacebar. When the game begins, a fleet
+fills the sky and moves across and down the screen. The player shoots and 
+destroys the aliens. If the player destroys all the aliens, a new fleet appears
+that moves faster than the previous fleet. If any alien hits the players ship
+or reaches the bottom of the screen, the player loses a ship. If the player
+loses three ships, the game ends. 
---
+++ src/extraterrestrial_incursion.py
@@ -0,0 +1,43 @@
+import sys
+
+import pygame
+
+from settings import Settings
+
+class ExtraterrestrialIncursion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)) # Called
+        # a surface in pygame! The surface returned by display.set_mode()
+        # represents the entire game window.
+        pygame.display.set_caption("Extraterrestrial Incursion")
+
+        # Set the background color.
+        self.bg_color = (230, 230, 230)
+    
+    def run_game(self):
+        """Start the main loop for a game."""
+        while True:
+            # Watch for the keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Redraw the screen during each passthrough the loop.
+            self.screen.fill(self.settings.bg_color)
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+            self.clock.tick(60)
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ei = ExtraterrestrialIncursion()
+    ei.run_game()
