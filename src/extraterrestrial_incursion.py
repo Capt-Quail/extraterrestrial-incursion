b1286eb0cec50f9cf3e9ac7da075c991e6431c10
Capt-Quail <146376347+Capt-Quail@users.noreply.github.com>
2024-03-25 19:18:59 -0400

    Refactored check_events() and update_screen() into helper methods.

--- src/extraterrestrial_incursion.py
+++ src/extraterrestrial_incursion.py
@@ -26,20 +26,24 @@ class ExtraterrestrialIncursion:
     def run_game(self):
         """Start the main loop for a game."""
         while True:
-            # Watch for the keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            # Redraw the screen during each passthrough the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
+          
             self.clock.tick(60)
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
     ei = ExtraterrestrialIncursion()
     ei.run_game()
+
+def _check_events(self):
+    """Respond to krypress and mouse events."""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+def _update_screen(self):
+    """Update images on the screen, and flip to the new screen."""
+    self.screen.fill(self.settings.bg_color)
+    self.ship.blitme()
+    pygame.display.flip()
