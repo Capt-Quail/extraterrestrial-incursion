e3b0c55a08fcbaade84f1e156bbc60f9e2738728
Capt-Quail <146376347+Capt-Quail@users.noreply.github.com>
2024-04-01 23:18:08 -0400

    Added 'quit' shortcut Kydown event for reqistering 'q' as sys.exit().

--- src/extraterrestrial_incursion.py
+++ src/extraterrestrial_incursion.py
@@ -47,6 +47,8 @@ class ExtraterrestrialIncursion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _check_keyup_events(self, event):
         """Respond to key release."""
