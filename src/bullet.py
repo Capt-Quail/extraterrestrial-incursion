619fc49b236a0fc5342226ca5579de1d2aed069f
Capt-Quail <146376347+Capt-Quail@users.noreply.github.com>
2024-04-09 23:11:20 -0400

    Added bullet visibility and movement Created the update() amd draw_bullet() methods to manage the bullet's position and visibility respectively

--- src/bullet.py
+++ src/bullet.py
@@ -18,3 +18,14 @@ class Bullet(Sprite):
 
         #Store the bullet's position as a float.
         self.y = float(self.rect.y)
+
+    def update(self):
+        """Move the bullet up the screen."""
+        #Update the exact position of the bullet.
+        self.y -= self.settings.bullet_speed
+        # Update the rect position.
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Draw the bullet to the screen."""
+        pygame.draw.rect(self.screen, self.color, self.rect)
