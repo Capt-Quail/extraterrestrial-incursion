a8bf8fb9ec41d1b3f1e1e45fd06a751c22780bee
Capt-Quail <146376347+Capt-Quail@users.noreply.github.com>
2024-04-16 22:42:54 -0400

    Fixed bullet color error in settings.py A mislabeled object caused the game to crash when firing a bullet with spacebar. This fix should equip enlisted defense force to fire efficiently.
    - Correct object name 'bullet_collor' to
      'bullet_color'

--- src/settings.py
+++ src/settings.py
@@ -15,4 +15,4 @@ class Settings:
         self.bullet_speed = 2.0
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_collor = (60, 60, 60)
+        self.bullet_color = (60, 60, 60)
