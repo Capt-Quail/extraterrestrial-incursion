67222f197f14d9b0c2244486d5c807c176780816
Capt-Quail <146376347+Capt-Quail@users.noreply.github.com>
2024-04-23 20:25:18 -0400

    Corrected initialization attribute Corrected attribute 'ai_game' to 'ei_game' for Alien class in alien.py.

--- src/alien.py
+++ src/alien.py
@@ -4,7 +4,7 @@ from pygame.sprite import Sprite
 class Alien(Sprite):
     """A class to represent a single alien in the fleet."""
 
-    def __init__(self, ai_game):
+    def __init__(self, ei_game):
         """Initialize the alien and set its starting position."""
         super().__init__()
         self.screen = ei_game.screen
