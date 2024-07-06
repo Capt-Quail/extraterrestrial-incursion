0a1a28558f3839a9903e76600e639fb051d079d0
Capt-Quail <146376347+Capt-Quail@users.noreply.github.com>
2024-07-05 23:05:10 -0400

    Refactoring _update_bullets in ei.py
    - Refactored _update_bullets() and created
      _check_bullet_alien_collisions() to respond to
      in-game bullet-alien in ei.py

--- src/extraterrestrial_incursion.py
+++ src/extraterrestrial_incursion.py
@@ -118,8 +118,11 @@ class ExtraterrestrialIncursion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
         
-        # Check for any bullets that have hit aliens.
-        #   If so, get rid of the bullet and the alien.
+        self._check_bullet_alien_collisions()
+    
+    def _check_bullet_alien_collisions(self):
+        """Respond to bullet-alien collisions."""
+        # Remove any bullets and aliens that have collided.
         collisions = pygame.sprite.groupcollide(
                 self.bullets, self.aliens, True, True)
         
