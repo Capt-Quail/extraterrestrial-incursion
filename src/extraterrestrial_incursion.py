93807083550cece4c5e793f202ff512874b1a374
Capt-Quail <146376347+Capt-Quail@users.noreply.github.com>
2024-07-07 03:02:30 -0400

    Add high score Added cumulative highscore for each completed level

--- src/extraterrestrial_incursion.py
+++ src/extraterrestrial_incursion.py
@@ -158,6 +158,7 @@ class ExtraterrestrialIncursion:
             for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
+            self.sb.check_high_score()
 
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
--- src/game_stats.py
+++ src/game_stats.py
@@ -5,6 +5,9 @@ class GameStats:
         """Initialize game statistics."""
         self.settings = ei_game.settings
         self.reset_stats()
+        
+        # High score should never be reset.
+        self.high_score = 0
     
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
